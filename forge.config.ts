c0b7c20a43d8979ef2ccf4ee5f89b95eb059a5d7
thealmightyderpybird <73889377+ajcoolcat@users.noreply.github.com>
2024-10-30 20:09:27 -0400

    vite

--- forge.config.ts
+++ forge.config.ts
@@ -3,43 +3,58 @@ import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
-
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import { VitePlugin } from '@electron-forge/plugin-vite';
+import { FusesPlugin } from '@electron-forge/plugin-fuses';
+import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const config: ForgeConfig = {
-  packagerConfig: {
-    asar: true,
-    icon: './favicon.ico'
-  },
-  rebuildConfig: {},
-  makers: [new MakerSquirrel({
-    iconUrl: "https://croomssched.cyclic.app/favicon.ico",
-    setupExe: "CroomsBellSetup.exe", setupIcon: "./installer.ico",
-    loadingGif: "./loader.gif", 
-  }), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
-  plugins: [
-    new AutoUnpackNativesPlugin({}),
-    new WebpackPlugin({
-      mainConfig,
-      devContentSecurityPolicy: "connect-src 'self' * 'unsafe-eval'",
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
-            preload: {
-              js: './src/preload.ts',
-            },
-          },
-        ],
-      },
-    }),
-  ],
+    packagerConfig: {
+        asar: true,
+        icon: './favicon.ico'
+    },
+    rebuildConfig: {},
+    makers: [new MakerSquirrel({
+        iconUrl: "https://croomssched.tech/favicon.ico",
+        setupExe: "CroomsBellSetup.exe", setupIcon: "./installer.ico",
+        loadingGif: "./loader.gif",
+    }), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+    plugins: [
+        new VitePlugin({
+            // `build` can specify multiple entry builds,
+            // which can be Main process, Preload scripts, Worker process, etc.
+            // If you are familiar with Vite configuration, it will look really familiar.
+            build: [
+                {
+                    // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+                    entry: 'src/main.ts',
+                    config: 'vite.main.config.ts',
+                    target: 'main',
+                },
+                {
+                    entry: 'src/preload.ts',
+                    config: 'vite.preload.config.ts',
+                    target: 'preload',
+                },
+            ],
+            renderer: [
+                {
+                    name: 'main_window',
+                    config: 'vite.renderer.config.ts',
+                },
+            ],
+        }),
+        // Fuses are used to enable/disable various Electron functionality
+        // at package time, before code signing the application
+        new FusesPlugin({
+            version: FuseVersion.V1,
+            [FuseV1Options.RunAsNode]: false,
+            [FuseV1Options.EnableCookieEncryption]: true,
+            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+            [FuseV1Options.EnableNodeCliInspectArguments]: false,
+            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+            [FuseV1Options.OnlyLoadAppFromAsar]: true,
+        }),
+    ],
 };
 
 export default config;
